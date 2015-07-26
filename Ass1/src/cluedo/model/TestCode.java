1d8ba2a82aeb2d931903e0605f193dfa4234f053
Cameron Bryers <cambo_the_rambo@hotmail.com>
2015-07-26 15:36:11 +1200

    Initial test

---
+++ Ass1/src/cluedo/model/TestCode.java
@@ -0,0 +1,22 @@
+package cluedo.model;
+
+public class TestCode {
+
+}
+
+abstract class Card {
+	
+}
+
+class Character extends Card {
+	
+}
+
+class Location extends Card {
+	
+}
+
+class Item extends Card {
+	
+}
+
