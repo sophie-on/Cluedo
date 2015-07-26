871c2ff0b2a9e511c2d07cb47bb283b3ad741a92
hc09141 <hannahcraighead23@gmail.com>
2015-07-26 21:16:08 +1200

    Dice

---
+++ Ass1/src/cluedo/model/gameObjects/Dice.java
@@ -0,0 +1,16 @@
+package cluedo.model.gameObjects;
+
+/**
+ * This item is used turn by turn by players and rolled
+ * to determine how far they are allowed to move their token
+ * @author Hannah
+ *
+ */
+
+public class Dice {
+	
+	public int roll(){
+		return (int)(1 + Math.random()*5);
+	}
+	
+}
