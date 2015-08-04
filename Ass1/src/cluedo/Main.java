fea5dafbf1158731627deecf0cb7ce1f2e45585e
Cameron Bryers <cambo_the_rambo@hotmail.com>
2015-08-05 10:11:03 +1200

    Main can run game again

--- Ass1/src/cluedo/Main.java
+++ Ass1/src/cluedo/Main.java
@@ -4,15 +4,13 @@ import cluedo.model.Game;
 
 /**
  * Main class, runs the entire game
- * 
+ *
  * @author Cameron Bryers, Hannah Craighead.
  *
  */
 public class Main {
 
 	public static void main(String args[]) {
-		char a = 'a';
-		System.out.println((int)a);
-		// Game cluedo = new Game();
+		Game cluedo = new Game();
 	}
 }
